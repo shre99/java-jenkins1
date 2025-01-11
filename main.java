b02299880fa824729f421ab6b996d707fb927949
shre99 <115724095+shre99@users.noreply.github.com>
2025-01-11 20:44:07 +0530

    update me

---
+++ main.java
@@ -0,0 +1,13 @@
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("This is JAva");
+        System.out.println("my new program");
+        System.out.println(Today's Date());
+        System.out.println("here we gooo...");
+        System.out.println("added one more file...");
+    }
+
+}
