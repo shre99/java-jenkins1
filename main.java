60693209fc9a526a3a23da39b0892cdf1178e54a
shre99 <115724095+shre99@users.noreply.github.com>
2025-01-11 20:51:54 +0530

    Update main.java

--- main.java
+++ main.java
@@ -5,7 +5,7 @@ public class Main {
 
         System.out.println("This is JAva");
         System.out.println("my new program");
-        System.out.println(Today's Date());
+        System.out.println("Today's date: " + new Date());
         System.out.println("here we gooo...");
         System.out.println("added one more file...");
     }
